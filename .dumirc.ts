d02514ba507a7e4d231dd90fae1b481bdfb8b192
RegMs If <me@regmsif.cf>
2023-08-30 13:10:32 +0800

    chore: update dumi base and public path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/x-star-utils/',
+  pubilcPath: '/x-star-utils/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'x-star-utils',
