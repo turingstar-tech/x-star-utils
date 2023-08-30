95547705eda7354b412d42e937f8a6c4404f983a
RegMs If <me@regmsif.cf>
2023-08-30 13:25:35 +0800

    fix: typo

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: '/x-star-utils/',
-  pubilcPath: '/x-star-utils/',
+  publicPath: '/x-star-utils/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'x-star-utils',
