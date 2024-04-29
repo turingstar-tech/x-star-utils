7819c9672e6cb743814432d605e03a7706717b18
RegMs If <me@regmsif.cf>
2024-04-29 18:02:33 +0800

    test: add empty string test

--- test/get-trans-result.test.ts
+++ test/get-trans-result.test.ts
@@ -5,13 +5,13 @@ describe('get trans result test', () => {
   test('success test', () => {
     //中文环境
     expect(getTransResult('zh', '测试', 'test')).toBe('测试');
-    expect(getTransResult('zh', undefined, 'test')).toBe('test');
-    expect(getTransResult('zh', '测试', undefined)).toBe('测试');
+    expect(getTransResult('zh', '', 'test')).toBe('test');
+    expect(getTransResult('zh', '测试', '')).toBe('测试');
     expect(getTransResult('zh', undefined, undefined)).toBe('');
     //英文环境
     expect(getTransResult('en', '测试', 'test')).toBe('test');
-    expect(getTransResult('en', undefined, 'test')).toBe('test');
-    expect(getTransResult('en', '测试', undefined)).toBe('测试');
+    expect(getTransResult('en', '', 'test')).toBe('test');
+    expect(getTransResult('en', '测试', '')).toBe('测试');
     expect(getTransResult('en', undefined, undefined)).toBe('');
   });
 });
